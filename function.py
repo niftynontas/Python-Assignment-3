52ce2f3712dc3eb8289e610349b781f8e40c914c
niftynontas <nontandomyoli1@gmail.com>
2025-04-01 22:27:31 +0200

    Add files via upload

---
+++ function.py
@@ -0,0 +1,6 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discounted_price = price - (price * (discount_percent / 100))
+        return discounted_price
+    else:
+        return price
